dabaa50666249e044290883e8edc8696ea9ca4cd
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-02 23:26:35 -0500

    done with use case for use state hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React, { useState, useCallback } from 'react'
+import List from './List'
 
 function App() {
+  const [dark, setDark] = useState(false)
+  const [number, setNumber] = useState(1)
+
+  const theme = {
+    backgroundColor: dark ? '#333' : '#FFF',
+    color: dark ? '#FFF' : '#333',
+  }
+
+  const toggleThemeHandler = () => {
+    console.log('hello')
+    setDark((prevDark) => !prevDark)
+  }
+
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2]
+  }, [number])
+
   return (
-    <h1>useCallback Hook Practice</h1>
-  );
+    <div style={theme}>
+      <h1>useCallback Hook Practice</h1>
+      <input
+        onChange={(event) => setNumber(parseInt(event.target.value || 0))}
+        type="number"
+        value={number || ''}
+      />
+      <button onClick={toggleThemeHandler}>Toggle Theme</button>
+      <List getItems={getItems}></List>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/List.js
@@ -0,0 +1,17 @@
+
+import React, {useState, useEffect} from 'react'
+const List = ({getItems}) => {
+
+    const [items, setItems] = useState([])
+
+    useEffect(()=>{
+        setItems(getItems())
+        console.log("updating items")
+    }, [getItems])
+    
+    return(
+        items.map(item => <div key={item}>{item}</div>)
+    )
+}
+
+export default List
